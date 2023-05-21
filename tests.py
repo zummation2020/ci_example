ce1cdb4341c164eddcb343f5d2e8ee4d4d244f0c
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-05-21 09:40:21 -0400

    test failure

--- tests.py
+++ tests.py
@@ -8,6 +8,10 @@ class TestCase(unittest.TestCase):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
 
+    def test2(self):
+        expected = "Hola World"
+        self.assertEqual(task.my_func(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
