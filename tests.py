f597573ee19e68123fcdbfd007f05b509a45c783
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2023-05-21 09:38:15 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,13 @@
 import unittest
+import task
 
-if __name__ == '__main__':
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+
+  if __name__ == '__main__':
     unittest.main()
